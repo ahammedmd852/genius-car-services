725eacb0b9edd56dd1e1036f217e9131b39adaec
ahammed <ahammedmd852@gmail.com>
2022-04-13 00:28:37 +0600

    navvar added

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import expert1 from '../../../images/expert-1.jpg';
-import expert2 from '../../../images/expert-2.jpg';
-import expert3 from '../../../images/expert-3.jpg';
-import expert4 from '../../../images/expert-4.jpg';
-import expert5 from '../../../images/expert-5.jpg';
-import expert6 from '../../../images/expert-6.png';
+import expert1 from '../../../images/experts/expert-1.jpg';
+import expert2 from '../../../images/experts/expert-2.jpg';
+import expert3 from '../../../images/experts/expert-3.jpg';
+import expert4 from '../../../images/experts/expert-4.jpg';
+import expert5 from '../../../images/experts/expert-5.jpg';
+import expert6 from '../../../images/experts/expert-6.png';
 import Expert from '../Expert/Expert';
 
 const Experts = () => {
@@ -18,7 +18,7 @@ const Experts = () => {
     ]
     return (
         <div className='container'>
-            <h1 className='text-primary text-center'>Our Experts</h1>
+            <h1 className='text-primary text-center mt-5'>Our Experts</h1>
             <div className='row'>
                 {
                     experts.map(expert => <Expert
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            <p>Copyright @</p>
+        <div className='text-center mt-5'>
+            <p>Copyright @ {year}</p>
         </div>
     );
 };
