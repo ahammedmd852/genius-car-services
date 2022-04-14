722dca71913ae732e83d45d5b6614049d4690297
ahammed <ahammedmd852@gmail.com>
2022-04-15 00:06:51 +0600

    require auth compeleted

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -4,7 +4,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const RequireAuth = ({ children }) => {
-    const [user] useAuthState(auth);
+    const [user] = useAuthState(auth);
     const location = useLocation();
 
     if (!user) {
