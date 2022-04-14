8161ada761eb4b06118e0bf14c152d7b48f00976
ahammed <ahammedmd852@gmail.com>
2022-04-14 21:50:38 +0600

    working with register component

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -41,7 +41,7 @@ const Login = () => {
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
-                <p className='mt-2'>New to Genius Car? <span onClick={navigateRegister} className='text-danger'>Please Register</span></p>
+                <p className='mt-2'>New to Genius Car? <Link to="/register" onClick={navigateRegister} className='text-danger text-decoration-none pe-auto'>Please Register</Link></p>
             </Form>
         </div>
     );
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login')
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Please Register</h1>
+        <div className='register-form'>
+            <h1 className='form-title'>Please Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email Address' required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input type="submit" className='register-btn text-white' value="Register" />
+            </form>
+            <p className='mt-2'>Already have an Account? <Link to="/login" onClick={navigateLogin} className='text-danger text-decoration-none pe-auto'>Please Login</Link></p>
         </div>
     );
 };
