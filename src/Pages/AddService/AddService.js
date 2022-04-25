fa2195021b0d390e4cc1e96ffb2a2125de8c7593
ahammed <ahammedmd852@gmail.com>
2022-04-25 22:33:29 +0600

    Add services componants has been added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const uri = "http://localhost:5000/service";
+        fetch(uri, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+            });
+    };
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h1>Please Book your service</h1>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder='Name' className='mb-2' {...register("name", { required: true, maxLength: 20 })} />
+                <textarea placeholder='Description' className='mb-2' {...register("description")} />
+                <input placeholder='Price' className='mb-2' type="number" {...register("price")} />
+                <input placeholder='Photo URL' className='mb-2' type="text" {...register("img")} />
+                <input type="submit" value="Add Services" />
+            </form>
         </div>
     );
 };
