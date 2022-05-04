37aabfd9e22ebe12798e80214814a4fde9626b80
ahammed <ahammedmd852@gmail.com>
2022-05-04 23:56:09 +0600

    in order component jwt access added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TitlePage from '../../Shared/TitlePage/TitlePage';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -36,15 +37,18 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
     const navigateRegister = event => {
         navigate('/register');
